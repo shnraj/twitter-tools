a4fdc176e03326b38ed067af96aa53a6b1719aae
Sahana Rajasekar <shnraj@gmail.com>
2016-03-16 14:15:12 -0700

    Find list of users that retweeted a tweet

--- twit.py
+++ twit.py
@@ -1,4 +1,5 @@
 import config  # file that contains my API token
+import json
 import oauth2 as oauth
 import oauth2 as urllib
 
@@ -20,7 +21,28 @@ print req(url="https://api.twitter.com/1.1/statuses/user_timeline.json",
           method="GET",
           data={"screen_name": "dan_abramov"})
 
+
+def follow_user(user_id=None, screen_name=None):
+    if user_id:
+        data = {"user_id": user_id}
+    if screen_name:
+        data = {"screen_name": screen_name}
+    return req(
+        url="https://api.twitter.com/1.1/friendships/create.json",
+        method="POST",
+        data=data)
+
 # Follow user
-print req(url="https://api.twitter.com/1.1/friendships/create.json",
-          method="POST",
-          data={"screen_name": "thatguyBG"})
+print follow_user(screen_name="thatguyBG")
+
+
+# Find users who retweeted my medium post
+def find_retweet_users(tweet_id):
+    data = {"count": 100, "id": tweet_id, "stringify_ids": "true"}
+    response = req(
+        url="https://api.twitter.com/1.1/statuses/retweeters/ids.json",
+        method="GET",
+        data=data)
+    return json.loads(response)["ids"]
+
+print find_retweet_users(tweet_id=707652131038351360)
